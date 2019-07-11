bf7240e9e432d84e70bc1122290ccdef6b08be2c
Natalie Reinicke <natalie@mjdinteractive.com>
2019-07-11 10:41:39 -0700

    adding more test;switching to syntax

--- cypress/integration/test_spec.js
+++ cypress/integration/test_spec.js
@@ -10,12 +10,16 @@ describe('go to site', () => {
 		cy.scrollTo('center')
 		cy.contains('Accessories').click()
 		cy.contains('Hats').click()
-		// cy.location().should((loc) => {
-		// 	expect(loc.url).to.eq('https://www.target.com/c/hats-men-s-accessories/-/N-55ktr')
-		// })
+		cy.location().should((loc) => {
+			expect(loc.href).to.include('/c/hats-men-s-accessories/-/N-55ktr')
+		})
 		cy.get('[type=checkbox]').eq(0).check({force: true })
 		cy.get('[type=checkbox]').eq(1).check({force: true })
 		cy.wait(1200)
+		cy.location().should((loc) => {
+			expect(loc.href).to.include('c/hats-men-s-accessories/-/N-55ktrZ5zl7wZ5zkty?storeIdList=201&Nao=0')
+		})
 		cy.get('[label="in stores"]').click()
+		cy.get('button').click()
 	})
 })
